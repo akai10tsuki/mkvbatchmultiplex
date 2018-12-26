3070a4c14874b8d2ea54363e7532de497b7f0095
Efrain Vergara <akai10tsuki@gmail.com>
2018-12-26 13:31:26 -0500

    RTD testing 	new file:   requirements.txt trying this again 	modified:   source/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+#sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 sys.path.insert(0, os.path.abspath('../..'))
 
