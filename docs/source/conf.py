1317f7c64313bd7f789e135fe783310dee8d9728
Efrain Vergara <akai10tsuki@gmail.com>
2018-12-26 12:51:18 -0500

    modified:   source/conf.py added mock modules for RTD install

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,16 @@
 #
 import os
 import sys
+from unittest.mock import MagicMock
+
+MOCK_MODULES = ['pyqt5', 'pymediainfo']
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 sys.path.insert(0, os.path.abspath('../..'))
 
