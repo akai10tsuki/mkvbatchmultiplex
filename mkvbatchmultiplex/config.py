f0b294908c36f71d35fc8b39175323ea6a5325a7
Efrain Vergara <akai10tsuki@gmail.com>
2019-01-01 19:22:43 -0500

    __version__ rename to comfig

---
+++ mkvbatchmultiplex/config.py
@@ -0,0 +1,8 @@
+"""mkvbatchmultiplex version info"""
+
+__VERSION = (0, 5, '3a2', 'dev3')
+
+VERSION = ".".join(map(str, __VERSION))
+AUTHOR = "Efrain Vergara"
+EMAIL = "akai10tsuki@gmail.com"
+QT_VERSION = "PYSIDE2"
