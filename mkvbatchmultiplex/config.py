72534d1f39469129eeae57a4480db9c0d2439978
Efrain Vergara <akai10tsuki@gmail.com>
2019-06-12 01:43:52 -0400

    chg: bump version

--- mkvbatchmultiplex/config.py
+++ mkvbatchmultiplex/config.py
@@ -13,7 +13,7 @@ from vsutillib.log import LogRotateFileHandler
 
 data = ConfigurationSettings() # pylint: disable=invalid-name
 
-__VERSION = (0, 9, '1b1', 'dev1')
+__VERSION = (0, 9, '2b1', 'dev1')
 
 APPNAME = "MKVBatchMultiplex"
 VERSION = ".".join(map(str, __VERSION))
@@ -40,7 +40,7 @@ PROJECTURLS = {
 PYTHONVERSIONS = '>=3.5, <=3.8'
 REQUIRED = [
     'pymediainfo>=4.0',
-    'PySide2>=5.12'
+    'PySide2>=5.12',
 ]
 
 # label
