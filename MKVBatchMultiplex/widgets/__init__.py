a5c679de82cb238b57498910dabb416a1259e5fe
Efrain Vergara <akai10tsuki@gmail.com>
2020-01-29 17:34:37 -0500

    add: TabsWidget import reference

--- MKVBatchMultiplex/widgets/__init__.py
+++ MKVBatchMultiplex/widgets/__init__.py
@@ -2,5 +2,7 @@
 widget package imports
 """
 
+from .CommandWidget import CommandWidget
 from .JobsTableView import JobsTableView
 from .JobsTableViewWidget import JobsTableViewWidget
+from .TabsWidget import TabsWidget
