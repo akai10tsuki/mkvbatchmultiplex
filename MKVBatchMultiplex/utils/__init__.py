56d095497bc777d21afcd7fb97668ff34ed90ced
Efrain Vergara <akai10tsuki@gmail.com>
2020-01-31 23:09:10 -0500

    add: OutputWindow import refernce

--- MKVBatchMultiplex/utils/__init__.py
+++ MKVBatchMultiplex/utils/__init__.py
@@ -4,10 +4,11 @@ utils package imports
 # UT0008
 
 
-from .text import Text
+from .mainSetLanguage import setLanguageMenus
+from .OutputWindows import OutputWindows
 from .populate import populate
 from .progress import Progress
-from .yesNoDialog import yesNoDialog
-from .ValidateCommand import ValidateCommand
-from .mainSetLanguage import setLanguageMenus
 from .SetLanguage import SetLanguage
+from .text import Text
+from .ValidateCommand import ValidateCommand
+from .yesNoDialog import yesNoDialog
