413ee842b387cdd3a37c2e790ee53784a6558d31
Efrain Vergara <akai10tsuki@gmail.com>
2020-02-19 00:14:42 -0500

    add: locale stings for RenameWidgets

--- genPotFile.bat
+++
@@ -1,3 +0,0 @@
-
-xgettext  -L Python -s -o .\jobstable\locale\jobstable.pot -kS_ .\jobsTable\utils\text.py
-xgettext  -L Python -j -s -kS_ -o .\jobstable\locale\jobstable.pot  .\jobsTable\jobsTable.py
