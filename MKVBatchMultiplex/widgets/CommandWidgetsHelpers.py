6f46d37293ead3c4241418d317061a5f63966ca0
Efrain Vergara <akai10tsuki@gmail.com>
2020-04-29 14:31:14 -0400

    chg: adjust for new MKVCommandParser

--- MKVBatchMultiplex/widgets/CommandWidgetsHelpers.py
+++ MKVBatchMultiplex/widgets/CommandWidgetsHelpers.py
@@ -16,6 +16,30 @@ def runAnalysis(**kwargs):
     output = kwargs.pop("output", None)
     command = kwargs.pop("command", None)
     log = kwargs.pop("log", False)
+    if command:
+        output.command.emit("Analysis of command line:\n", {LineOutput.AppendEnd: True})
+        verify = mkv.MKVCommandParser(command, log=log)
+        verify.generateCommands()
+        for e in verify.analysis:
+            if e.find(r"chk:") >= 0:
+                output.command.emit(
+                    "{}".format(e),
+                    {LineOutput.Color: SvgColor.darkgreen, LineOutput.AppendEnd: True},
+                )
+            else:
+                output.command.emit(
+                    "{}".format(e),
+                    {LineOutput.Color: SvgColor.red, LineOutput.AppendEnd: True},
+                )
+        output.command.emit("\n", {LineOutput.AppendEnd: True})
+
+
+def runAnalysisOld(**kwargs):
+    """List the source files found"""
+
+    output = kwargs.pop("output", None)
+    command = kwargs.pop("command", None)
+    log = kwargs.pop("log", False)
     verify = mkv.VerifyMKVCommand(command, log=log)
     output.command.emit("Analysis of command line:\n", {LineOutput.AppendEnd: True})
 
@@ -41,9 +65,10 @@ def showCommands(**kwargs):
     command = kwargs.pop("command", None)
     oCommand = kwargs.pop("oCommand", None)
     log = kwargs.pop("log", False)
+    index = 1
 
     if oCommand is None:
-        oCommand = mkv.MKVCommand(command, log=log)
+        oCommand = mkv.MKVCommandParser(command, log=log)
 
     if output is None:
         return "No output callback function"
@@ -51,14 +76,23 @@ def showCommands(**kwargs):
     output.command.emit(
         "Shell:\n\n{}\n".format(oCommand.command), {LineOutput.AppendEnd: True}
     )
+
+    if not oCommand.commandsGenerated:
+        output.command.emit("Generating Commands...\n", {LineOutput.AppendEnd: True})
+        oCommand.generateCommands()
+
     output.command.emit(
-        "Command Template:\n\n{}\n\nCommands:\n".format(str(oCommand.template)),
+        "Command Template:\n\n{}\n\nCommands:\n".format(str(oCommand.commandTemplate)),
         {LineOutput.AppendEnd: True},
     )
 
     if oCommand:
-        for command, _, _, _, _ in oCommand:
-            output.command.emit(str(command) + "\n", {LineOutput.AppendEnd: True})
+        for command, _, _, _, _, _, _ in oCommand:
+            output.command.emit(
+                "Command {}.\n{}".format(index, str(command)) + "\n",
+                {LineOutput.AppendEnd: True},
+            )
+            index += 1
     else:
         output.command.emit(
             "MCW0008: Error in command construction {}\n".format(oCommand.error),
@@ -79,17 +113,21 @@ def checkFiles(**kwargs):
     log = kwargs.pop("log", False)
 
     if oCommand is None:
-        oCommand = mkv.MKVCommand(command, log=log)
+        oCommand = mkv.MKVCommandParser(command, log=log)
 
     if output is None:
         return "No output callback function"
 
     output.command.emit("Checking files...\n", {LineOutput.AppendEnd: True})
 
+    if not oCommand.commandsGenerated:
+        output.command.emit("Generating Commands...\n", {LineOutput.AppendEnd: True})
+        oCommand.generateCommands()
+
     if oCommand:
         verify = mkv.VerifyStructure(log=log)
 
-        for index, (_, baseFiles, sourceFiles, destinationFile, _) in enumerate(
+        for index, (_, baseFiles, sourceFiles, destinationFile, _, _, _) in enumerate(
             oCommand
         ):
             verify.verifyStructure(baseFiles, sourceFiles)
