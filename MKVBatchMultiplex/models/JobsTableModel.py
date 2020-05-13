bb1ecffcb08ce3761f689ba92fda74590a03c1a6
Efrain Vergara <akai10tsuki@gmail.com>
2020-05-13 12:58:46 -0400

    chg: added color to status column

--- MKVBatchMultiplex/models/JobsTableModel.py
+++ MKVBatchMultiplex/models/JobsTableModel.py
@@ -7,6 +7,8 @@ Class for a table model with horizontal headers
 
 from PySide2.QtCore import Qt, QModelIndex
 
+from vsutillib.pyqt import checkColor, SvgColor
+
 from ..jobs import JobStatus, JobKey
 from .TableModel import TableModel
 
@@ -27,6 +29,60 @@ class JobsTableModel(TableModel):
         self.jobQueue = jobQueue
         self.model = model
 
+    #
+    # Read-Only
+    #
+
+    def data(self, index, role):
+        """
+        TableModel.data() override.
+
+        Use by views and delegates to get table information.
+
+        Arguments:
+            index {int} - QModelIndex object that point to a call
+            role {int} - what type of data to return. Roles include:
+                Qt.DisplayRole, Qt.EditRole, Qt.TextAlignmentRole, Qt.DecorationRole,
+                Qt.ToolTipRole, Qt.StatusTipRole, Qt.FontRole, Qt.BackgroundRole
+                Qt.ForgroundRole
+        """
+
+        if index.isValid():
+            if role == Qt.ForegroundRole:
+                value = self.dataset.data[index.row()][index.column()].data
+                if value == JobStatus.Done:
+                    return SvgColor.cyan
+                elif value == JobStatus.Running:
+                    return SvgColor.green
+                elif value in [
+                    JobStatus.Abort,
+                    JobStatus.Aborted,
+                    JobStatus.AbortForced,
+                    JobStatus.AbortJob,
+                    JobStatus.AbortJobError,
+                    JobStatus.Stop,
+                    JobStatus.Stopped,
+                ]:
+                    return SvgColor.red
+                elif value in [
+                    JobStatus.Blocked,
+                    JobStatus.DoneWithError,
+                    JobStatus.Error,
+                ]:
+                    return SvgColor.tomato
+                elif value in [JobStatus.Skip, JobStatus.Skipped]:
+                    return SvgColor.yellow
+                elif value in [JobStatus.Waiting]:
+                    return SvgColor.yellowgreen
+
+        superReturn = super().data(index, role)
+
+        return superReturn
+
+    #
+    # Resizable Model
+    #
+
     def insertRows(self, position, rows, index=QModelIndex(), data=None):
 
         rc = super(JobsTableModel, self).insertRows(position, rows, index, data=data)
@@ -38,6 +94,10 @@ class JobsTableModel(TableModel):
             self.jobQueue.statusChangeSignal.emit(index)
 
         return rc
+
+    #
+    # Editable
+    #
 
     def setData(self, index, value, role=Qt.EditRole):
         """
