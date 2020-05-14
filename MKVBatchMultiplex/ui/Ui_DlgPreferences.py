57e22a605be2344e958ffd3ae39521a2d277768b
Efrain Vergara <akai10tsuki@gmail.com>
2020-05-14 09:54:20 -0400

    chg: rename ui file

--- MKVBatchMultiplex/ui/Ui_DlgPreferences.py
+++ MKVBatchMultiplex/ui/Ui_DlgPreferences.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'preferences.ui'
+## Form generated from reading UI file 'DlgPreferences.ui'
 ##
 ## Created by: Qt User Interface Compiler version 5.14.2
 ##
