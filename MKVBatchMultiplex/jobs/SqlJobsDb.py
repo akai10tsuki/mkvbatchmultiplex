5e052d326359b6b80c0d5edb9b7d7e68db4bb1f0
Efrain Vergara <akai10tsuki@gmail.com>
2020-05-16 18:01:54 -0400

    add: 'update' method and rework fetchJob method

--- MKVBatchMultiplex/jobs/SqlJobsDb.py
+++ MKVBatchMultiplex/jobs/SqlJobsDb.py
@@ -4,7 +4,15 @@
 
 from vsutillib.sql import SqlDb
 
-class JobsDB(SqlDb):
+
+class SqlJobsDB(SqlDb):
+    """
+    SqlJobsDB sqlite database for jobs history
+
+    Args:
+        SqlDb (class): class for managing sqlite connections
+    """
+
     def __init__(self, dbFile=None):
         super().__init__(dbFile)
 
@@ -21,7 +29,6 @@ class JobsDB(SqlDb):
                                 job BLOB
                                 ) """
             self.sqlExecute(sqlJobsTable)
-            print("create table")
 
     def connect(self, database, autoCommit=False):
         super().connect(database, autoCommit)
@@ -29,6 +36,15 @@ class JobsDB(SqlDb):
         self._initHelper()
 
     def delete(self, jobID):
+        """
+        delete job from database
+
+        Args:
+            jobID (int): job id to delete
+
+        Returns:
+            sqlite3.cursor: cursor to the database aftter operation
+        """
 
         cursor = None
         if isinstance(jobID, int):
@@ -40,6 +56,15 @@ class JobsDB(SqlDb):
         return cursor
 
     def insert(self, *args):
+        """
+        insert job into database
+
+        Args:
+            jobID (int): job id to insert
+
+        Returns:
+            sqlite3.cursor: cursor to the database after operation
+        """
 
         sqlJob = """ INSERT INTO
                      jobs(id, addDate, addTime, startTime, endTime, job)
@@ -50,7 +75,7 @@ class JobsDB(SqlDb):
 
         return cursor
 
-    def fetchJob(self, jobID, fields=None, fetchAll=False):
+    def fetchJob(self, jobID, *args, fetchAll=False, whereClause=None):
         """
         fetchJob fetch information from database by job id
 
@@ -60,20 +85,100 @@ class JobsDB(SqlDb):
             fetchAll (bool, optional): fetch all records in query. Defaults to False.
 
         Returns:
-            [type]: [description]
+            sqlite3.cursor: cursor to the operation results
         """
 
-        cursor = None
-        if isinstance(jobID, int):
-
-            if jobID <= 0:
-                sqlFechID = "SELECT * FROM jobs"
-                cursor = self.sqlExecute(sqlFechID)
+        fetchFields = ""
+        wClause = ""
+        values = ""
+        if isinstance(jobID, dict):
+            strTmp = ""
+            wClause = "WHERE "
+            totalFields = len(jobID)
+            values = []
+            for i, (f, v) in enumerate(jobID.items()):
+                strTmp += f + " = ?"
+                if i < (totalFields - 1):
+                    strTmp += " AND "
+                values.append(v)
+            wClause += strTmp
+            if args:
+                strTmp = ""
+                totalFields = len(args)
+                for i, f in enumerate(args):
+                    fetchFields += f
+                    if i < (totalFields - 1):
+                        fetchFields += ", "
             else:
-                sqlFechID = "SELECT * FROM jobs WHERE id=?"
-                cursor = self.sqlExecute(sqlFechID, jobID)
+                fetchFields = "*"
+        elif isinstance(jobID, int):
+            if jobID <= 0:
+                fetchFields = "*"
+                wClause = ""
+            else:
+                fetchFields = "*"
+                wClause = "WHERE id = ?"
+                values = [jobID]
 
-            if fetchAll:
-                cursor.fetchall()
+        cursor = None
+        if whereClause is not None:
+            wClause = whereClause
+
+        sqlFetchID = (
+            "SELECT " + fetchFields + " FROM jobs" + (""
+            if wClause == ""
+            else " " + wClause)
+        )
+        print(sqlFetchID)
+        print("values = {}".format(values))
+
+        if values:
+            cursor = self.sqlExecute(sqlFetchID, *values)
+        else:
+            cursor = self.sqlExecute(sqlFetchID)
+
+        if fetchAll:
+            cursor.fetchall()
+
+        return cursor
+
+    def update(self, jobID, fields, *args):
+        """
+        update job information on database
+
+        fields = (id, addDate, addTime, startTime, endTime, job)
+
+        Args:
+            jobID (int): job id to update
+            fields (tuple): fields to update
+            args (tuple): values to update
+        """
+
+        sqlUpdateJob = "UPDATE jobs SET "
+        values = []
+        totalFields = len(fields)
+        strTmp = ""
+
+        for i, f in enumerate(fields):
+            strTmp += f + " = ?"
+            if i < (totalFields - 1):
+                strTmp += ", "
+
+        sqlUpdateJob += strTmp + " WHERE id = ?"
+
+        print("args", args)
+
+        for v in args:
+            values.append(v)
+
+        values.append(jobID)
+
+        cursor = None
+
+        if isinstance(jobID, int) and isinstance(fields, tuple):
+
+            cursor = self.sqlExecute(sqlUpdateJob, *values)
+            if cursor is not None:
+                self.connection.commit()
 
         return cursor
