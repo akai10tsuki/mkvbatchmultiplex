8d81821776f00c6273b8e2cfed12ed0be04bbb0b
Efrain Vergara <akai10tsuki@gmail.com>
2020-05-18 01:49:54 -0400

    chg: update dependency

--- MKVBatchMultiplex/widgets/__init__.py
+++ MKVBatchMultiplex/widgets/__init__.py
@@ -4,6 +4,8 @@ widget package imports
 
 from .CommandWidget import CommandWidget
 from .JobsHistoryViewWidget import JobsHistoryViewWidget
+from .JobsOutputErrorsWidget import JobsOutputErrorsWidget
+from .JobsOutputWidget import JobsOutputWidget
 from .JobsTableView import JobsTableView
 from .JobsTableViewWidget import JobsTableViewWidget
 from .PreferencesDialogWidget import PreferencesDialogWidget
