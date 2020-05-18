394123825efa53371f9401eae7955405f3ce8ee1
Efrain Vergara <akai10tsuki@gmail.com>
2020-05-17 20:25:19 -0400

    chg: all columns fields can have tooltips

--- MKVBatchMultiplex/models/JobsTableModel.py
+++ MKVBatchMultiplex/models/JobsTableModel.py
@@ -9,7 +9,8 @@ from PySide2.QtCore import Qt, QModelIndex
 
 from vsutillib.pyqt import checkColor, SvgColor
 
-from ..jobs import JobStatus, JobKey
+from ..dataset import TableData, tableHeaders
+from ..jobs import JobStatus, JobKey, jobStatusTooltip
 from .TableModel import TableModel
 
 # JOBID, JOBSTATUS, JOBCOMMAND = range(3)
@@ -48,32 +49,41 @@ class JobsTableModel(TableModel):
         """
 
         if index.isValid():
+            row = index.row()
+            column = index.column()
+
             if role == Qt.ForegroundRole:
-                value = self.dataset.data[index.row()][index.column()].data
-                if value == JobStatus.Done:
-                    return SvgColor.cyan
-                elif value == JobStatus.Running:
-                    return SvgColor.green
-                elif value in [
-                    JobStatus.Abort,
-                    JobStatus.Aborted,
-                    JobStatus.AbortForced,
-                    JobStatus.AbortJob,
-                    JobStatus.AbortJobError,
-                    JobStatus.Stop,
-                    JobStatus.Stopped,
-                ]:
-                    return SvgColor.red
-                elif value in [
-                    JobStatus.Blocked,
-                    JobStatus.DoneWithError,
-                    JobStatus.Error,
-                ]:
-                    return SvgColor.tomato
-                elif value in [JobStatus.Skip, JobStatus.Skipped]:
-                    return SvgColor.yellow
-                elif value in [JobStatus.Waiting]:
-                    return SvgColor.yellowgreen
+                if column == JobKey.Status:
+                    value = self.dataset.data[row][column].data
+                    if value == JobStatus.Done:
+                        return SvgColor.cyan
+                    elif value == JobStatus.Running:
+                        return SvgColor.green
+                    elif value in [
+                        JobStatus.Abort,
+                        JobStatus.Aborted,
+                        JobStatus.AbortForced,
+                        JobStatus.AbortJob,
+                        JobStatus.AbortJobError,
+                        JobStatus.Stop,
+                        JobStatus.Stopped,
+                    ]:
+                        return SvgColor.red
+                    elif value in [
+                        JobStatus.Blocked,
+                        JobStatus.DoneWithError,
+                        JobStatus.Error,
+                    ]:
+                        return SvgColor.tomato
+                    elif value in [JobStatus.Skip, JobStatus.Skipped]:
+                        return SvgColor.yellow
+                    elif value in [JobStatus.Waiting]:
+                        return SvgColor.yellowgreen
+
+            if role == Qt.ToolTipRole:
+                if column == JobKey.Status:
+                    value = self.dataset.data[row][column].data
+                    return jobStatusTooltip(value)
 
         superReturn = super().data(index, role)
 
