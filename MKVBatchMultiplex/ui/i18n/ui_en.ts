a83f2dbfcf4c8d84ab35d2f7eb1e284158ffad38
Efrain Vergara <akai10tsuki@gmail.com>
2020-05-19 23:07:56 -0400

    add: spanish language for Preferences dialogs - ui_en.qm, ui_es.qm compiled translation files - ui_es.ts, ui_en.ts translation strings - UiSetLanguage to set the correct locale files
    new file:   MKVBatchMultiplex/locale/en/LC_MESSAGES/ui_en.qm
    new file:   MKVBatchMultiplex/locale/es/LC_MESSAGES/ui_es.qm
    new file:   MKVBatchMultiplex/ui/dialogs.pro
    new file:   MKVBatchMultiplex/ui/i18n/ui_en.ts
    new file:   MKVBatchMultiplex/ui/i18n/ui_es.ts
    new file:   MKVBatchMultiplex/utils/UiSetLanguage.py

---
+++ MKVBatchMultiplex/ui/i18n/ui_en.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>PreferencesDialog</name>
+    <message>
+        <location filename="PreferencesDialog.ui" line="22"/>
+        <source>Preferences</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="PreferencesDialog.ui" line="50"/>
+        <source>Options</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="PreferencesDialog.ui" line="65"/>
+        <source>Interface Language:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="PreferencesDialog.ui" line="88"/>
+        <source>Enable Logging</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="PreferencesDialog.ui" line="95"/>
+        <source>Enable jobs history</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="PreferencesDialog.ui" line="102"/>
+        <source>Restore original window size</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="PreferencesDialog.ui" line="75"/>
+        <source>Font &amp; Size:</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
---
+++ MKVBatchMultiplex/ui/i18n/ui_es.ts
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+<context>
+    <name>PreferencesDialog</name>
+    <message>
+        <location filename="PreferencesDialog.ui" line="22"/>
+        <source>Preferences</source>
+        <translation>Preferencias</translation>
+    </message>
+    <message>
+        <location filename="PreferencesDialog.ui" line="50"/>
+        <source>Options</source>
+        <translation>Opciones</translation>
+    </message>
+    <message>
+        <location filename="PreferencesDialog.ui" line="65"/>
+        <source>Interface Language:</source>
+        <translation>Idioma del interfaz:</translation>
+    </message>
+    <message>
+        <location filename="PreferencesDialog.ui" line="75"/>
+        <source>Font &amp; Size</source>
+        <translation type="obsolete">Fuente y tamaño</translation>
+    </message>
+    <message>
+        <location filename="PreferencesDialog.ui" line="88"/>
+        <source>Enable Logging</source>
+        <translation>Habilitar Registro</translation>
+    </message>
+    <message>
+        <location filename="PreferencesDialog.ui" line="95"/>
+        <source>Enable jobs history</source>
+        <translation>Habilitar historial de tareas</translation>
+    </message>
+    <message>
+        <location filename="PreferencesDialog.ui" line="102"/>
+        <source>Restore original window size</source>
+        <translation>Restaurar tamaño de ventana original</translation>
+    </message>
+    <message>
+        <location filename="PreferencesDialog.ui" line="75"/>
+        <source>Font &amp; Size:</source>
+        <translation>Fuente y tamaño:</translation>
+    </message>
+</context>
+</TS>
