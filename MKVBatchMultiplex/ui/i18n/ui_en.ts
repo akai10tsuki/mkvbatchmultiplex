6cebebb07919e23a890ae071d36f08d3ba7fdab8
Efrain Vergara <akai10tsuki@gmail.com>
2020-05-24 06:10:53 -0400

    add: ui locale phrases

--- MKVBatchMultiplex/ui/i18n/ui_en.ts
+++ MKVBatchMultiplex/ui/i18n/ui_en.ts
@@ -14,33 +14,51 @@
         <translation></translation>
     </message>
     <message>
-        <location filename="PreferencesDialog.ui" line="65"/>
+        <location filename="PreferencesDialog.ui" line="68"/>
         <source>Interface Language:</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="PreferencesDialog.ui" line="88"/>
+        <location filename="PreferencesDialog.ui" line="91"/>
         <source>Enable Logging</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="PreferencesDialog.ui" line="95"/>
+        <location filename="PreferencesDialog.ui" line="98"/>
         <source>Enable jobs history</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="PreferencesDialog.ui" line="102"/>
+        <location filename="PreferencesDialog.ui" line="105"/>
         <source>Restore original window size</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="PreferencesDialog.ui" line="75"/>
+        <location filename="PreferencesDialog.ui" line="78"/>
         <source>Font &amp; Size:</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="PreferencesDialog.ui" line="119"/>
+        <location filename="PreferencesDialog.ui" line="122"/>
         <source>Restore Defaults</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>SearchTextDialog</name>
+    <message>
+        <location filename="SearchTextDialog.ui" line="19"/>
+        <source>Search Text</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="SearchTextDialog.ui" line="92"/>
+        <source>Search</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="SearchTextDialog.ui" line="65"/>
+        <source>Text to search:</source>
         <translation></translation>
     </message>
 </context>
--- MKVBatchMultiplex/ui/i18n/ui_es.ts
+++ MKVBatchMultiplex/ui/i18n/ui_es.ts
@@ -14,7 +14,7 @@
         <translation>Opciones</translation>
     </message>
     <message>
-        <location filename="PreferencesDialog.ui" line="65"/>
+        <location filename="PreferencesDialog.ui" line="68"/>
         <source>Interface Language:</source>
         <translation>Idioma del interfaz:</translation>
     </message>
@@ -24,29 +24,47 @@
         <translation type="obsolete">Fuente y tamaño</translation>
     </message>
     <message>
-        <location filename="PreferencesDialog.ui" line="88"/>
+        <location filename="PreferencesDialog.ui" line="91"/>
         <source>Enable Logging</source>
         <translation>Habilitar Registro</translation>
     </message>
     <message>
-        <location filename="PreferencesDialog.ui" line="95"/>
+        <location filename="PreferencesDialog.ui" line="98"/>
         <source>Enable jobs history</source>
         <translation>Habilitar historial de tareas</translation>
     </message>
     <message>
-        <location filename="PreferencesDialog.ui" line="102"/>
+        <location filename="PreferencesDialog.ui" line="105"/>
         <source>Restore original window size</source>
         <translation>Restaurar tamaño de ventana original</translation>
     </message>
     <message>
-        <location filename="PreferencesDialog.ui" line="75"/>
+        <location filename="PreferencesDialog.ui" line="78"/>
         <source>Font &amp; Size:</source>
         <translation>Fuente y tamaño:</translation>
     </message>
     <message>
-        <location filename="PreferencesDialog.ui" line="119"/>
+        <location filename="PreferencesDialog.ui" line="122"/>
         <source>Restore Defaults</source>
         <translation>Restaurar los valores predeterminados</translation>
+    </message>
+</context>
+<context>
+    <name>SearchTextDialog</name>
+    <message>
+        <location filename="SearchTextDialog.ui" line="19"/>
+        <source>Search Text</source>
+        <translation>Búsqueda</translation>
+    </message>
+    <message>
+        <location filename="SearchTextDialog.ui" line="92"/>
+        <source>Search</source>
+        <translation>Buscar</translation>
+    </message>
+    <message>
+        <location filename="SearchTextDialog.ui" line="65"/>
+        <source>Text to search:</source>
+        <translation>Texto a buscar:</translation>
     </message>
 </context>
 </TS>
