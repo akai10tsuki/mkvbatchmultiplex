fd4775f1ce80d2979b1a4bd93b4ee601619cbec1
Efrain Vergara <akai10tsuki@gmail.com>
2020-05-24 05:37:48 -0400

    chg: code refactoring

--- MKVBatchMultiplex/jobs/RunJobs.py
+++ MKVBatchMultiplex/jobs/RunJobs.py
@@ -14,6 +14,7 @@ from vsutillib.pyqt import SvgColor
 from .. import config
 from ..models import TableProxyModel
 
+from .jobsWorker import jobsWorker
 
 MODULELOG = logging.getLogger(__name__)
 MODULELOG.addHandler(logging.NullHandler())
