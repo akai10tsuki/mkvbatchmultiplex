c58582af9556a156f1ea42cb117dd8a7cfa84da0
Efrain Vergara <akai10tsuki@gmail.com>
2020-06-18 11:18:29 -0400

    add: QSystemTrayIcon dependency

--- MKVBatchMultiplex/utils/__init__.py
+++ MKVBatchMultiplex/utils/__init__.py
@@ -10,6 +10,7 @@ from .populate import populate
 from .preferences import preferences, SetPreferences
 from .PreferencesDialogUI import PreferencesDialogUI
 from .progress import Progress
+from .QSystemTrayIconWidget import QSystemTrayIconWidget
 from .SetLanguage import SetLanguage
 from .text import Text
 from .UiSetLanguage import UiSetLanguage
