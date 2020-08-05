178b6246a3bad7a5c5cc079ef832c4e55ca39c73
Efrain Vergara <akai10tsuki@gmail.com>
2020-08-05 01:53:44 -0400

    bump version

--- MKVBatchMultiplex/config.py
+++ MKVBatchMultiplex/config.py
@@ -14,7 +14,7 @@ from vsutillib.files import ConfigurationSettings
 from vsutillib.log import LogRotateFileHandler
 from vsutillib.pyqt import QSignalLogHandler
 
-__VERSION = (2, 0, "0a2")
+__VERSION = (2, 0, "0a2dev1")
 
 APPNAME = "MKVBatchMultiplex"
 VERSION = ".".join(map(str, __VERSION))
