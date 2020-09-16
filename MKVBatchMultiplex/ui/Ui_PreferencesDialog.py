d6351a780bb989dbcd8d19ad0e7669aed105b91c
Efrain Vergara <akai10tsuki@gmail.com>
2020-09-15 23:08:12 -0400

    add: Algorithm selection

--- MKVBatchMultiplex/ui/Ui_PreferencesDialog.py
+++ MKVBatchMultiplex/ui/Ui_PreferencesDialog.py
@@ -3,16 +3,13 @@
 ################################################################################
 ## Form generated from reading UI file 'PreferencesDialog.ui'
 ##
-## Created by: Qt User Interface Compiler version 5.15.0
+## Created by: Qt User Interface Compiler version 5.15.1
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide2.QtCore import (QCoreApplication, QDate, QDateTime, QMetaObject,
-    QObject, QPoint, QRect, QSize, QTime, QUrl, Qt)
-from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
-    QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter,
-    QPixmap, QRadialGradient)
+from PySide2.QtCore import *
+from PySide2.QtGui import *
 from PySide2.QtWidgets import *
 
 
@@ -20,19 +17,19 @@ class Ui_PreferencesDialog(object):
     def setupUi(self, PreferencesDialog):
         if not PreferencesDialog.objectName():
             PreferencesDialog.setObjectName(u"PreferencesDialog")
-        PreferencesDialog.resize(744, 495)
+        PreferencesDialog.resize(744, 507)
         font = QFont()
         font.setPointSize(14)
         PreferencesDialog.setFont(font)
         PreferencesDialog.setContextMenuPolicy(Qt.DefaultContextMenu)
         self.btnBox = QDialogButtonBox(PreferencesDialog)
         self.btnBox.setObjectName(u"btnBox")
-        self.btnBox.setGeometry(QRect(460, 390, 241, 32))
+        self.btnBox.setGeometry(QRect(460, 430, 241, 32))
         self.btnBox.setOrientation(Qt.Horizontal)
         self.btnBox.setStandardButtons(QDialogButtonBox.Cancel|QDialogButtonBox.Ok)
         self.grpBox = QGroupBox(PreferencesDialog)
         self.grpBox.setObjectName(u"grpBox")
-        self.grpBox.setGeometry(QRect(30, 40, 681, 321))
+        self.grpBox.setGeometry(QRect(30, 40, 681, 361))
         self.lblInterfaceLanguage = QLabel(self.grpBox)
         self.lblInterfaceLanguage.setObjectName(u"lblInterfaceLanguage")
         self.lblInterfaceLanguage.setGeometry(QRect(21, 42, 172, 23))
@@ -68,9 +65,26 @@ class Ui_PreferencesDialog(object):
         self.chkBoxAutoSaveJobHistory.setObjectName(u"chkBoxAutoSaveJobHistory")
         self.chkBoxAutoSaveJobHistory.setGeometry(QRect(45, 236, 611, 27))
         self.chkBoxAutoSaveJobHistory.setBaseSize(QSize(0, 0))
+        self.frame = QFrame(self.grpBox)
+        self.frame.setObjectName(u"frame")
+        self.frame.setGeometry(QRect(20, 310, 621, 41))
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.lblAlgorithm = QLabel(self.frame)
+        self.lblAlgorithm.setObjectName(u"lblAlgorithm")
+        self.lblAlgorithm.setGeometry(QRect(10, 0, 121, 41))
+        self.rbZero = QRadioButton(self.frame)
+        self.rbZero.setObjectName(u"rbZero")
+        self.rbZero.setGeometry(QRect(130, 0, 41, 41))
+        self.rbOne = QRadioButton(self.frame)
+        self.rbOne.setObjectName(u"rbOne")
+        self.rbOne.setGeometry(QRect(180, 0, 41, 41))
+        self.rbTwo = QRadioButton(self.frame)
+        self.rbTwo.setObjectName(u"rbTwo")
+        self.rbTwo.setGeometry(QRect(230, 0, 41, 41))
         self.btnRestoreDefaults = QPushButton(PreferencesDialog)
         self.btnRestoreDefaults.setObjectName(u"btnRestoreDefaults")
-        self.btnRestoreDefaults.setGeometry(QRect(50, 390, 351, 34))
+        self.btnRestoreDefaults.setGeometry(QRect(50, 430, 351, 34))
 
         self.retranslateUi(PreferencesDialog)
         self.btnBox.accepted.connect(PreferencesDialog.accept)
@@ -81,7 +95,7 @@ class Ui_PreferencesDialog(object):
 
     def retranslateUi(self, PreferencesDialog):
         PreferencesDialog.setWindowTitle(QCoreApplication.translate("PreferencesDialog", u"Preferences", None))
-        self.grpBox.setTitle(QCoreApplication.translate("PreferencesDialog", u"Options", None))
+        self.grpBox.setTitle("")
         self.lblInterfaceLanguage.setText(QCoreApplication.translate("PreferencesDialog", u"Interface Language:", None))
         self.chkBoxRestoreWindowSize.setText(QCoreApplication.translate("PreferencesDialog", u"Restore original window size", None))
         self.label.setText("")
@@ -90,6 +104,10 @@ class Ui_PreferencesDialog(object):
         self.chkBoxEnableJobHistory.setText(QCoreApplication.translate("PreferencesDialog", u"Enable jobs history", None))
         self.chkBoxEnableLogViewer.setText(QCoreApplication.translate("PreferencesDialog", u"Enable log viewer", None))
         self.chkBoxAutoSaveJobHistory.setText(QCoreApplication.translate("PreferencesDialog", u"Automatically save jobs", None))
+        self.lblAlgorithm.setText(QCoreApplication.translate("PreferencesDialog", u"Algorithm:", None))
+        self.rbZero.setText(QCoreApplication.translate("PreferencesDialog", u"0", None))
+        self.rbOne.setText(QCoreApplication.translate("PreferencesDialog", u"1", None))
+        self.rbTwo.setText(QCoreApplication.translate("PreferencesDialog", u"2", None))
         self.btnRestoreDefaults.setText(QCoreApplication.translate("PreferencesDialog", u"Restore Defaults", None))
     # retranslateUi
 
