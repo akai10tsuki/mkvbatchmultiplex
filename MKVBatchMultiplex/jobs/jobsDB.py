335d09fd946c3c5042a20a330d72b646a39e8c90
Efrain Vergara <akai10tsuki@gmail.com>
2020-09-29 01:34:04 -0400

    add: saveToDb

--- MKVBatchMultiplex/jobs/jobsDB.py
+++ MKVBatchMultiplex/jobs/jobsDB.py
@@ -12,6 +12,89 @@ import zlib
 from .. import config
 
 from .jobKeys import JobKey, JobsTableKey
+from .SqlJobsTable import SqlJobsTable
+
+def saveToDb(job, update=False, name=None, description=None):
+    """
+    saveToDb add the job to the history database
+
+    Args:
+        database (SqlJobsTable): history database
+        job (JobInfo): running job information
+        update (bool, optional): update is true if record should exits.
+            Defaults to False.
+
+    Returns:
+        int: rowid if insert successful. 0 otherwise.
+    """
+
+    # Compress job information:
+    # compressed = zlib.compress(cPickle.dumps(obj))
+
+    # Get it back:
+    # obj = cPickle.loads(zlib.decompress(compressed))
+
+    # Key ID, startTime
+    #
+
+    bSimulateRun = config.data.get(config.ConfigKey.SimulateRun)
+    rc = 0
+
+    if not bSimulateRun:
+        if name is None:
+            jobName = "AutoSave"
+        else:
+            jobName = name
+        if description is None:
+            jobDescription = "AutoSave"
+        else:
+            jobDescription = description
+        jobsDB = SqlJobsTable(config.data.get(config.ConfigKey.SystemDB))
+        cmpJob = zlib.compress(pickle.dumps(job))
+        if not update:
+            rowid = jobsDB.insert(
+                job.jobRow[JobKey.ID],
+                job.date.isoformat(),
+                job.addTime,
+                job.startTime,
+                job.endTime,
+                cmpJob,
+                job.oCommand.command,
+                jobName,
+                jobDescription,
+                0,
+                0,
+            )
+            rc = rowid
+
+            if rowid > 0:
+                sqlSearchUpdate = """
+                    INSERT INTO jobsSearch(rowidKey, id, startTime, command)
+                        VALUES(?, ?, ?, ?); """
+                jobsDB.sqlExecute(
+                    sqlSearchUpdate,
+                    rowid,
+                    job.jobRow[JobKey.ID],
+                    job.startTime,
+                    job.oCommand.command,
+                )
+            if rowid == 0:
+                print("error", jobsDB.error)
+                sys.exit()
+        else:
+            # jobsDB.update(449, (JobsTableKey.startTime, ), 80)
+            jobsDB.update(
+                job.jobRow[JobKey.ID],
+                (JobsTableKey.startTime, JobsTableKey.endTime, JobsTableKey.job),
+                job.startTime,
+                job.endTime,
+                cmpJob,
+            )
+        jobsDB.close()
+
+    return rc
+
+
 
 def addToDb(database, job, update=False):
     """
