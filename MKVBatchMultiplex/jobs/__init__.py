5f8e441be21cc9ad052868d08445af41f2d016df
Efrain Vergara <akai10tsuki@gmail.com>
2020-10-01 13:07:08 -0400

    add: saveToDb reference

--- MKVBatchMultiplex/jobs/__init__.py
+++ MKVBatchMultiplex/jobs/__init__.py
@@ -4,6 +4,6 @@ Import jobs module entry point
 
 from .jobKeys import JobHistoryKey, JobKey, JobStatus, JobsTableKey, jobStatusTooltip
 from .JobQueue import JobInfo, JobQueue
-from .jobsDB import addToDb, removeFromDb
+from .jobsDB import addToDb, removeFromDb, saveToDb
 from .RunJobs import RunJobs
 from .SqlJobsTable import SqlJobsTable
