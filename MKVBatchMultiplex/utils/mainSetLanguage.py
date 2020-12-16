623a3f40c997ff8d8377c0429c5909d3afb9b13d
Efrain Vergara <akai10tsuki@gmail.com>
2020-12-16 13:45:39 -0500

    new algorithm old one brakes with PySide6

--- MKVBatchMultiplex/utils/mainSetLanguage.py
+++ MKVBatchMultiplex/utils/mainSetLanguage.py
@@ -2,52 +2,38 @@
 set language locale for main window menus
 """
 
-from PySide2.QtWidgets import QAction
-
 from vsutillib.pyqt import QMenuWidget, QActionWidget
 
 from .. import config
 from .text import Text
 
-def setLanguageMenus(menuActions):
+
+def setLanguageMenus(parent):
     """
     setLanguageMenus set language locale on menu items
 
     Args:
         menuActions (QMenu): item to transverse to set labels in correct locale
     """
-    for m in menuActions:
-        # menus on menu bar
-        t = m.menu()
 
-        if isinstance(t, QMenuWidget):
-            t.setTitle(t.titlePrefix + _(t.originaltitle) + t.titleSuffix)
+    print("Setting Language.....")
 
-        for a in t.actions():
-            if a.isSeparator():
-                continue
-
-            if isinstance(a, QActionWidget):
-                if a.shortcut is not None:
-                    a.setShortcut(_(a.shortcut))
-
-                if a.tooltip is not None:
-                    if a.tooltip == Text.txt0020:
-                        msg = _(a.tooltip).format(
-                            "~/" + config.FILESROOT + "/" + config.LOGFILE
-                        )
-                        a.setStatusTip(msg)
-                    else:
-                        a.setStatusTip(_(a.tooltip))
-
-                if a.text is not None:
-                    a.setText(a.textPrefix + _(a.originaltext) + a.textSuffix)
-
-            elif isinstance(a, QMenuWidget):
-                a.setTitle(_(a.originaltitle))
-
-            elif isinstance(a, QAction):
-                i = a.menu()
-
-                if isinstance(t, QMenuWidget):
-                    i.setTitle(_(i.originaltitle))
+    for action in parent.menuItems:
+        if isinstance(action, QActionWidget):
+            action.setText(
+                action.textPrefix + _(action.originalText) + action.textSuffix
+            )
+            if action.statusTip:
+                action.setStatusTip(_(action.statusTip))
+            if action.toolTip:
+                if action.toolTip == Text.txt0020:
+                    msg = _(action.toolTip).format(
+                        "~/" + config.FILESROOT + "/" + config.LOGFILE
+                    )
+                    action.setStatusTip(msg)
+                else:
+                    action.setStatusTip(_(action.toolTip))
+        if isinstance(action, QMenuWidget):
+            action.setTitle(
+                action.titlePrefix + _(action.originalTitle) + action.titleSuffix
+            )
