93cd05b2b0aeca9c68d5b9d9e53717b6999eb9cf
Efrain Vergara <akai10tsuki@gmail.com>
2020-12-24 01:58:53 -0500

    position widgets

--- MKVBatchMultiplex/ui/Ui_ProjectInfoDialog.py
+++ MKVBatchMultiplex/ui/Ui_ProjectInfoDialog.py
@@ -3,7 +3,7 @@
 ################################################################################
 ## Form generated from reading UI file 'ProjectInfoDialog.ui'
 ##
-## Created by: Qt User Interface Compiler version 5.15.1
+## Created by: Qt User Interface Compiler version 5.15.2
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
@@ -14,40 +14,41 @@ from PySide2.QtWidgets import *
 
 
 class Ui_ProjectInfoDialog(object):
-    def setupUi(self, projectInfo):
-        if not projectInfo.objectName():
-            projectInfo.setObjectName(u"projectInfo")
-        projectInfo.resize(583, 394)
+    def setupUi(self, ProjectInfoDialog):
+        if not ProjectInfoDialog.objectName():
+            ProjectInfoDialog.setObjectName(u"ProjectInfoDialog")
+        ProjectInfoDialog.resize(583, 394)
         font = QFont()
         font.setPointSize(14)
-        projectInfo.setFont(font)
-        self.buttonBox = QDialogButtonBox(projectInfo)
+        ProjectInfoDialog.setFont(font)
+        self.buttonBox = QDialogButtonBox(ProjectInfoDialog)
         self.buttonBox.setObjectName(u"buttonBox")
         self.buttonBox.setGeometry(QRect(190, 350, 341, 32))
         self.buttonBox.setOrientation(Qt.Horizontal)
         self.buttonBox.setStandardButtons(QDialogButtonBox.Cancel|QDialogButtonBox.Ok)
-        self.lblName = QLabel(projectInfo)
+        self.lblName = QLabel(ProjectInfoDialog)
         self.lblName.setObjectName(u"lblName")
         self.lblName.setGeometry(QRect(31, 41, 511, 27))
-        self.teDescription = QTextEdit(projectInfo)
+        self.teDescription = QTextEdit(ProjectInfoDialog)
         self.teDescription.setObjectName(u"teDescription")
         self.teDescription.setGeometry(QRect(31, 134, 509, 186))
-        self.lblDescription = QLabel(projectInfo)
+        self.lblDescription = QLabel(ProjectInfoDialog)
         self.lblDescription.setObjectName(u"lblDescription")
         self.lblDescription.setGeometry(QRect(31, 105, 511, 27))
-        self.leName = QLineEdit(projectInfo)
+        self.leName = QLineEdit(ProjectInfoDialog)
         self.leName.setObjectName(u"leName")
         self.leName.setGeometry(QRect(31, 70, 511, 29))
+        QWidget.setTabOrder(self.leName, self.teDescription)
 
-        self.retranslateUi(projectInfo)
-        self.buttonBox.accepted.connect(projectInfo.accept)
-        self.buttonBox.rejected.connect(projectInfo.reject)
+        self.retranslateUi(ProjectInfoDialog)
+        self.buttonBox.accepted.connect(ProjectInfoDialog.accept)
+        self.buttonBox.rejected.connect(ProjectInfoDialog.reject)
 
-        QMetaObject.connectSlotsByName(projectInfo)
+        QMetaObject.connectSlotsByName(ProjectInfoDialog)
     # setupUi
 
-    def retranslateUi(self, projectInfo):
-        projectInfo.setWindowTitle(QCoreApplication.translate("ProjectInfoDialog", u"Project Information", None))
+    def retranslateUi(self, ProjectInfoDialog):
+        ProjectInfoDialog.setWindowTitle(QCoreApplication.translate("ProjectInfoDialog", u"Project Information", None))
         self.lblName.setText(QCoreApplication.translate("ProjectInfoDialog", u"Name", None))
         self.lblDescription.setText(QCoreApplication.translate("ProjectInfoDialog", u"Description", None))
         self.leName.setText("")
