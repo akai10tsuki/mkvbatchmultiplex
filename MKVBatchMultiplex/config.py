91755e815f46cfcf479ba436cecec5476e89d25b
Efrain Vergara <akai10tsuki@gmail.com>
2021-03-05 10:06:50 -0500

    type hints

--- MKVBatchMultiplex/config.py
+++ MKVBatchMultiplex/config.py
@@ -3,48 +3,53 @@ mkvbatchmultiplex config file
 """
 # CF0005
 
+# for app
+__VERSION__: tuple = (2, 1, "0b1", "dev5")
+__version__: str = ".".join(map(str, __VERSION__))
+
 import logging
 import os
 import sys
 
+from typing import Callable, ClassVar, Dict, List, Optional
 from pathlib import Path
 
 from PySide2.QtGui import QFont
+from PySide2.QtWidgets import QApplication
 
 from vsutillib.files import ConfigurationSettings
 from vsutillib.log import LogRotateFileHandler
 from vsutillib.pyqt import QSignalLogHandler
 
-__VERSION = (2, 1, "0b1", "dev5")
 
-APPNAME = "MKVBatchMultiplex"
-VERSION = ".".join(map(str, __VERSION))
+APPNAME: str = "MKVBatchMultiplex"
+VERSION: str = ".".join(map(str, __VERSION__))
 
-AUTHOR = "Efrain Vergara"
-EMAIL = "akai10tsuki@gmail.com"
+AUTHOR: str = "Efrain Vergara"
+EMAIL: str = "akai10tsuki@gmail.com"
 
 # for setup.py
-COPYRIGHT = "2018-2021, Efrain Vergara"
-LICENSE = "MIT"
-DESCRIPTION = "A mkv media batch multiplex."
-ENTRYPOINTS = {
+COPYRIGHT: str = "2018-2021, Efrain Vergara"
+LICENSE: str = "MIT"
+DESCRIPTION: str = "A mkv media batch multiplex."
+ENTRYPOINTS: Dict[str, List[str]] = {
     "console_scripts": [
         "mkvbatchmultiplex=MKVBatchMultiplex:mainApp",
     ],
 }
-KEYWORDS = "mkv multimedia video mkvtoolnix plex"
-NAME = APPNAME.lower()
-PACKAGES = [APPNAME]
-URL = "https://github.com/akai10tsuki/mkvbatchmultiplex"
-PROJECTURLS = {
+KEYWORDS: str = "mkv multimedia video mkvtoolnix plex"
+NAME: str = APPNAME.lower()
+PACKAGES: str = [APPNAME]
+URL: str = "https://github.com/akai10tsuki/mkvbatchmultiplex"
+PROJECTURLS: Dict[str, str] = {
     "Bug Reports": "https://github.com/akai10tsuki/mkvbatchmultiplex/issues",
     "Source": "https://github.com/akai10tsuki/mkvbatchmultiplex/",
 }
-PYTHONVERSIONS = ">=3.8.1, <4.0"
-QT_VERSION = "PYSIDE2"
-PYSIDE2_VERSION = ">=5.15.2"
-PYSIDE6_VERSION = ">=6.0.0"
-REQUIRED = [
+PYTHONVERSIONS: str = ">=3.8.1, <4.0"
+QT_VERSION: str = "PYSIDE2"
+PYSIDE2_VERSION: str = ">=5.15.2"
+PYSIDE6_VERSION: str = ">=6.0.0"
+REQUIRED: List[str] = [
     "PySide2>=5.15",
     "vsutillib-files>=1.6.5",
     "vsutillib-log>=1.6.0",
@@ -59,37 +64,39 @@ REQUIRED = [
 #    "vsutillib-macos>=1.6.1", # pulls process
 # ]
 
-# for app
-__version__ = VERSION
 
 # label
 
-CONFIGFILE = "config.xml"
-FILESROOT = "." + APPNAME
-LOGFILE = APPNAME + ".log"
-IMAGESFILESPATH = "images"
-DEFAULTLANGUAGE = "en"
+CONFIGFILE: str = "config.xml"
+FILESROOT: str = "." + APPNAME
+LOGFILE: str = APPNAME + ".log"
+IMAGESFILESPATH: str = "images"
+DEFAULTLANGUAGE: str = "en"
 
 if getattr(sys, "frozen", False):
     # Running in pyinstaller bundle
-    CWD = Path(os.path.dirname(__file__)).parent
+    CWD: Path = Path(os.path.dirname(__file__)).parent
 else:
-    CWD = Path(os.path.realpath(__file__)).parent
-LOCALE = CWD.joinpath("locale")
+    CWD: Path = Path(os.path.realpath(__file__)).parent
+LOCALE: str = CWD.joinpath("locale")
 
-data = ConfigurationSettings()  # pylint: disable=invalid-name
-logViewer = QSignalLogHandler()  # pylint: disable=invalid-name
+data: Callable[
+    [], ConfigurationSettings
+] = ConfigurationSettings()  # pylint: disable=invalid-name
+logViewer: Callable[
+    [], QSignalLogHandler
+] = QSignalLogHandler()  # pylint: disable=invalid-name
 
-FORCELOG = True
+FORCELOG: bool = True
 
 ######################
 # Application specific
 ######################
 
-WORKERTHREADNAME = "jobsWorker"
-SYSTEMDATABASE = "itsue.db"
-ALGORITHMDEFAULT = 1
-DATABASEVERSION = "2.1.0"
+WORKERTHREADNAME: str = "jobsWorker"
+SYSTEMDATABASE: str = "itsue.db"
+ALGORITHMDEFAULT: int = 1
+DATABASEVERSION: str = "2.1.0"
 
 #######################
 #######################
@@ -97,10 +104,10 @@ DATABASEVERSION = "2.1.0"
 
 class Action:  # pylint: disable=too-few-public-methods
 
-    Save = "Save"
-    Reset = "Reset"
-    Restore = "Restore"
-    Update = "Update"
+    Save: ClassVar[str] = "Save"
+    Reset: ClassVar[str] = "Reset"
+    Restore: ClassVar[str] = "Restore"
+    Update: ClassVar[str] = "Update"
 
 
 class ConfigKey:  # pylint: disable=too-few-public-methods
@@ -109,54 +116,66 @@ class ConfigKey:  # pylint: disable=too-few-public-methods
     will have
     """
 
-    DarkMode = "DarkMode"
-    DefaultGeometry = "DefaultGeometry"
-    Font = "Font"
-    Geometry = "Geometry"
-    InterfaceLanguages = "InterfaceLanguages"
-    Language = "Language"
-    Logging = "Logging"
-    SimulateRun = "SimulateRun"
-    SystemDB = "SystemDB"
-    SystemFont = "SystemFont"
+    DarkMode: ClassVar[str] = "DarkMode"
+    DefaultGeometry: ClassVar[str] = "DefaultGeometry"
+    Font: ClassVar[str] = "Font"
+    Geometry: ClassVar[str] = "Geometry"
+    InterfaceLanguages: ClassVar[str] = "InterfaceLanguages"
+    Language: ClassVar[str] = "Language"
+    Logging: ClassVar[str] = "Logging"
+    SimulateRun: ClassVar[str] = "SimulateRun"
+    SystemDB: ClassVar[str] = "SystemDB"
+    SystemFont: ClassVar[str] = "SystemFont"
 
     #
     # App Specific
     #
 
-    Algorithm = "Algorithm"
-    DbVersion = "DbVersion"
-    JobsAutoSave = "JobsAutoSave"
-    JobHistory = "JobHistory"
-    JobHistoryDisabled = "JobsHistoryDisabled"
-    JobID = "JobID"
-    JobsTable = "jobs"
-    LogViewer = "LogViewer"
-    Tab = "Tab"
-    TabText = "TabText"
-    TextSuffix = "TextSuffix"
+    Algorithm: ClassVar[str] = "Algorithm"
+    DbVersion: ClassVar[str] = "DbVersion"
+    JobsAutoSave: ClassVar[str] = "JobsAutoSave"
+    JobHistory: ClassVar[str] = "JobHistory"
+    JobHistoryDisabled: ClassVar[str] = "JobsHistoryDisabled"
+    JobID: ClassVar[str] = "JobID"
+    JobsTable: ClassVar[str] = "jobs"
+    LogViewer: ClassVar[str] = "LogViewer"
+    Tab: ClassVar[str] = "Tab"
+    TabText: ClassVar[str] = "TabText"
+    TextSuffix: ClassVar[str] = "TextSuffix"
 
 
 class Key:
 
-    RegEx = "RegEx"
-    SubString = "SubString"
-    MaxRegExCount = "MaxRegExCount"
+    RegEx: ClassVar[str] = "RegEx"
+    SubString: ClassVar[str] = "SubString"
+    MaxRegExCount: ClassVar[str] = "MaxRegExCount"
 
 
-def init(filesRoot=None, cfgFile=None, logFile=None, name=None, version=None, app=None):
+def init(
+    filesRoot: Optional[str] = None,
+    cfgFile: Optional[str] = None,
+    logFile: Optional[str] = None,
+    name: Optional[str] = None,
+    version: Optional[str] = None,
+    app: Optional[str] = None,
+):
     """
     configures the system to save application configuration to xml file
 
     Args:
-        filesRoot (str, optional): root folder on ~ for files. Defaults to [.vsutillib].
-        configFile (str, optional): name of configuration file. Defaults to [config.xml].
-        logFile (str, optional): name of logging file. Defaults to [vsutillib.log].
-        name (str, optional): name of application. Defaults to [vsutillib].
-        version (str, optional): appplication version . Defaults to [vsutillib version].
+        **filesRoot** (str, optional): root folder on ~ for files. Defaults to [.vsutillib].
+
+        **configFile** (str, optional): name of configuration file. Defaults to [config.xml].
+
+        **logFile** (str, optional): name of logging file. Defaults to [vsutillib.log].
+
+        **name** (str, optional): name of application. Defaults to [vsutillib].
+
+        **version** (str, optional): appplication version . Defaults to [vsutillib version].
     """
 
     if filesRoot is None:
+        # root path for configuration files
         filesPath = Path(Path.home(), FILESROOT)
     else:
         filesPath = Path(Path.home(), filesRoot)
@@ -164,6 +183,7 @@ def init(filesRoot=None, cfgFile=None, logFile=None, name=None, version=None, ap
     filesPath.mkdir(parents=True, exist_ok=True)
 
     if cfgFile is None:
+        # setup main configuration file
         configFile = Path(filesPath, CONFIGFILE)
     else:
         configFile = Path(filesPath, cfgFile)
@@ -173,14 +193,15 @@ def init(filesRoot=None, cfgFile=None, logFile=None, name=None, version=None, ap
 
     setDefaultFont(app)
 
+    #
+    # Setup logging
+    #
     if FORCELOG:
         data.set(ConfigKey.Logging, True)
 
-    if data.get(ConfigKey.LogViewer) is None:
-        data.set(ConfigKey.LogViewer, False)
+    data.set(ConfigKey.LogViewer, data.get(ConfigKey.LogViewer) or False)
 
-    if data.get(ConfigKey.Language) is None:
-        data.set(ConfigKey.Language, DEFAULTLANGUAGE)
+    data.set(ConfigKey.Language, data.get(ConfigKey.Language) or DEFAULTLANGUAGE)
 
     if logFile is None:
         loggingFile = Path(filesPath, LOGFILE)
@@ -195,15 +216,8 @@ def init(filesRoot=None, cfgFile=None, logFile=None, name=None, version=None, ap
     logging.getLogger("").addHandler(logViewer)
     logging.getLogger("").setLevel(logging.DEBUG)
 
-    if name is None:
-        appName = APPNAME
-    else:
-        appName = name
-
-    if version is None:
-        appVersion = VERSION
-    else:
-        appVersion = version
+    appName = name or APPNAME
+    appVersion = version or VERSION
 
     logging.info("CF0001: App Start.")
     logging.info("CF0002: Python: %s", sys.version)
@@ -221,49 +235,46 @@ def init(filesRoot=None, cfgFile=None, logFile=None, name=None, version=None, ap
     #
     setRegEx()
 
-    if data.get(ConfigKey.Algorithm) is None:
-        data.set(ConfigKey.Algorithm, 1)
+    data.set(ConfigKey.Algorithm, data.get(ConfigKey.Algorithm) or 1)
 
-    if data.get(ConfigKey.DbVersion) is None:
-        data.set(ConfigKey.DbVersion, DATABASEVERSION)
+    data.set(ConfigKey.DbVersion, data.get(ConfigKey.DbVersion) or DATABASEVERSION)
 
-    if data.get(ConfigKey.JobsAutoSave) is None:
-        data.set(ConfigKey.JobsAutoSave, False)
+    data.set(ConfigKey.JobsAutoSave, data.get(ConfigKey.JobsAutoSave) or False)
 
-    if data.get(ConfigKey.JobHistory) is None:
-        data.set(ConfigKey.JobHistory, False)
+    data.set(ConfigKey.JobHistory, data.get(ConfigKey.JobHistory) or False)
 
-    if data.get(ConfigKey.JobHistoryDisabled) is None:
-        data.set(ConfigKey.JobHistoryDisabled, False)
-    else:
-        data.set(ConfigKey.JobHistoryDisabled, False)
+    data.set(
+        ConfigKey.JobHistoryDisabled, data.get(ConfigKey.JobHistoryDisabled) or False
+    )
+    # data.set(ConfigKey.JobHistoryDisabled, False)
 
-    if data.get(Key.MaxRegExCount) is None:
-        data.set(Key.MaxRegExCount, 20)
+    data.set(Key.MaxRegExCount, data.get(Key.MaxRegExCount) or 20)
 
-    if data.get(ConfigKey.TextSuffix) is None:
-        data.set(ConfigKey.TextSuffix, (
-                    ".srt",
-                    ".ssa",
-                    ".ass",
-                    ".pgs",
-                    ".idx",
-                    ".vob",
-                    ".vtt",
-                    ".usf",
-                    ".dvb",
-                    ".smi",
-                )
-        )
+    data.set(
+        ConfigKey.TextSuffix,
+        data.get(ConfigKey.TextSuffix)
+        or (
+            ".srt",
+            ".ssa",
+            ".ass",
+            ".pgs",
+            ".idx",
+            ".vob",
+            ".vtt",
+            ".usf",
+            ".dvb",
+            ".smi",
+        ),
+    )
 
     #
     # Temporalily disable uncomment statements
     #
-    #data.set(ConfigKey.JobHistoryDisabled, True)
-    #data.set(ConfigKey.JobHistory, False)
+    # data.set(ConfigKey.JobHistoryDisabled, True)
+    # data.set(ConfigKey.JobHistory, False)
 
 
-def setDefaultFont(app):
+def setDefaultFont(app: QApplication) -> None:
     """save and set default font point size"""
 
     strSystemFont = data.get(ConfigKey.SystemFont)
@@ -281,22 +292,24 @@ def setDefaultFont(app):
         data.set(ConfigKey.Font, font.toString())
 
 
-def setDefaultGeometry():
+def setDefaultGeometry() -> None:
 
-    if data.get(ConfigKey.DefaultGeometry) is None:
-        data.set(ConfigKey.DefaultGeometry, (0, 0, 1280, 720))
-
-
-def setInterfaceLanguage():
-
-    if data.get(ConfigKey.InterfaceLanguages) is None:
-        data.set(
-            ConfigKey.InterfaceLanguages,
-            {"en": "English (Inglés)", "es": "Español (Spanish)"},
-        )
+    data.set(
+        ConfigKey.DefaultGeometry,
+        data.get(ConfigKey.DefaultGeometry) or (0, 0, 1280, 720),
+    )
 
 
-def setRegEx():
+def setInterfaceLanguage() -> None:
+
+    data.set(
+        ConfigKey.InterfaceLanguages,
+        data.get(ConfigKey.InterfaceLanguages)
+        or {"en": "English (Inglés)", "es": "Español (Spanish)"},
+    )
+
+
+def setRegEx() -> None:
     """Update regex to 2.0.0a2"""
 
     oldRegEx = [
@@ -367,12 +380,12 @@ def setRegEx():
     data.set(Key.MaxRegExCount, 20)
 
 
-def close():
+def close() -> None:
     """exit accounting"""
 
     data.saveToFile()
     logging.info("CF0004: App End.")
 
 
-def logTest(msg):
+def logTest(msg: str) -> None:
     print(msg)
