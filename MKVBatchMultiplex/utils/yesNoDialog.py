f009b1c960b3d27f42297c19b8b3148ae62cac0b
akai10tuki <akai10tsuki@gmail.com>
2022-01-29 02:32:54 -0500

    Dialog with yes/no option works with locale

---
+++ MKVBatchMultiplex/utils/yesNoDialog.py
@@ -0,0 +1,31 @@
+"""
+QMessageBox Yes/No dialog.
+"""
+
+from PySide6.QtWidgets import QMessageBox, QWidget
+
+from .Text import Text
+
+
+def yesNoDialog(parent: QWidget, msg: str, title: str):
+    """
+    Convenience function to display a Yes/No dialog
+
+    Returns:
+        bool: return True if yes button press. No otherwise
+    """
+
+    m = QMessageBox(parent)
+    m.setText(msg)
+    m.setIcon(QMessageBox.Question)
+    yesButton = m.addButton(_(Text.txt0082), QMessageBox.ButtonRole.YesRole)
+    noButton = m.addButton(" No ", QMessageBox.ButtonRole.NoRole)
+    m.setDefaultButton(noButton)
+    m.setFont(parent.font())
+    m.setWindowTitle(title)
+    m.exec_()
+
+    if m.clickedButton() == yesButton:
+        return True
+
+    return False
