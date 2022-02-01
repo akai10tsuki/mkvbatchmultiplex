5b2c6652c5fc3105a989914dc5c6ac7d406333ac
akai10tuki <akai10tsuki@gmail.com>
2022-02-01 00:31:54 -0500

    Import updates

--- MKVBatchMultiplex/widgets/__init__.py
+++ MKVBatchMultiplex/widgets/__init__.py
@@ -3,4 +3,6 @@ widget package imports
 """
 
 from .CommandWidget import CommandWidget
+from .JobsOutputErrorsWidget import JobsOutputErrorsWidget
+from .JobsOutputWidget import JobsOutputWidget
 from .PreferencesDialogWidget import PreferencesDialogWidget
