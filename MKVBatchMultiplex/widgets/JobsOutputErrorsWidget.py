5161df0c6c41a198ec4077eab72e31fcf800828d
akai10tuki <akai10tsuki@gmail.com>
2022-02-01 00:37:33 -0500

    Widget to dispay error messages

---
+++ MKVBatchMultiplex/widgets/JobsOutputErrorsWidget.py
@@ -0,0 +1,29 @@
+"""
+JobsOutputWidget
+"""
+
+from typing import Optional, Any
+
+from PySide6.QtWidgets import QWidget
+
+from vsutillib.pyside6 import QOutputTextWidget, TabWidgetExtension
+
+
+class JobsOutputErrorsWidget(TabWidgetExtension, QOutputTextWidget):
+
+    def __init__(
+        self, 
+        parent: QWidget, 
+        log: Optional[bool] = None, 
+        **kwargs: Any):
+        super().__init__(parent=parent, log=log, **kwargs)
+
+"""
+        self.parent = parent
+
+    def clear(self):
+
+        self.parent.progress.lblSetValue.emit(4, 0)
+
+        super().clear()
+"""
