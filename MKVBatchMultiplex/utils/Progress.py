14ba6cd3fae6405c68088e6a6325fbd0758abb81
akai10tuki <akai10tsuki@gmail.com>
2022-04-18 01:23:10 -0400

    Progress status code

---
+++ MKVBatchMultiplex/utils/Progress.py
@@ -0,0 +1,86 @@
+"""
+Define different signals for progress updates
+"""
+
+from typing import Optional
+
+from PySide6.QtCore import QObject, Signal
+from PySide6.QtWidgets import QWidget
+
+from vsutillib.pyqt import DualProgressBar, QFormatLabel
+
+
+class Progress(QObject):
+    """
+    Progress class to connect Signals to DualProgressBar and QFormatLabel classes
+
+    Args:
+        parent (QWidget): parent widget
+        progressBar (DualProgressBar): progress bar widget
+        formatLabel (QFormatLabel, optional): label with text defined as
+                                             a format string.
+            Defaults to None.
+    """
+
+    pbReset = Signal()
+    pbSetAlignment = Signal(int)
+    pbSetValues = Signal(int, int)
+    pbSetMaximum = Signal(int, int)
+    pbSetMinimum = Signal(int, int)
+    pbSetRange = Signal(int, int, int, int)
+    lblSetValues = Signal(list)
+    lblSetValue = Signal(int, object)
+
+    def __init__(
+            self,
+            parent: QWidget,
+            progressBar: QWidget,
+            formatLabel: Optional[QWidget] = None) -> None:
+        super(Progress, self).__init__()
+
+        self.parent = parent
+        self.pb = progressBar
+        self.lbl = formatLabel
+
+        if formatLabel is not None:
+            self._initHelper(True, True)
+        else:
+            self._initHelper(True, False)
+
+    def _initHelper(
+            self,
+            initProgressBar: Optional[bool] = False,
+            initLabel: Optional[bool] = False) -> None:
+
+        if initProgressBar:
+
+            self.pbReset.connect(self.pb.reset)
+            self.pbSetAlignment.connect(self.pb.setAlignment)
+            self.pbSetValues.connect(self.pb.setValues)
+            self.pbSetMaximum.connect(self.pb.setMaximum)
+            self.pbSetMinimum.connect(self.pb.setMinimum)
+            self.pbSetRange.connect(self.pb.setRange)
+
+        if initLabel:
+
+            self.lblSetValue.connect(self.lbl.setValue)
+            self.lblSetValues.connect(self.lbl.setValues)
+
+    @property
+    def progressBar(self) -> DualProgressBar:
+        return self.pb
+
+    @progressBar.setter
+    def progressBar(self, value) -> None:
+
+        if isinstance(value, DualProgressBar):
+            self.pb = value
+
+    @property
+    def formatLabel(self) -> QFormatLabel:
+        return self.lbl
+
+    @formatLabel.setter
+    def formatLabel(self, value):
+        if isinstance(value, QFormatLabel):
+            self.lbl = value
