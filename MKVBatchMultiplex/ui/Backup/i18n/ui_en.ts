59f634791c78862b195fe8126805a7e5def75281
akai10tuki <akai10tsuki@gmail.com>
2024-01-31 00:27:20 -0500

    update phrases for designer dialogs

--- MKVBatchMultiplex/ui/Backup/i18n/ui_en.ts
+++ MKVBatchMultiplex/ui/Backup/i18n/ui_en.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="es_ES">
+<TS version="2.1" language="en_US">
 <context>
     <name>PreferencesDialog</name>
     <message>
@@ -10,7 +10,7 @@
         <location filename="../Backup/PreferencesDialog.3.0.Beta0.ui" line="22"/>
         <location filename="../PreferencesDialog.ui" line="22"/>
         <source>Preferences</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../Backup/PreferencesDialog-2.0.0.ui" line="50"/>
@@ -25,16 +25,7 @@
         <location filename="../Backup/PreferencesDialog.3.0.Beta0.ui" line="62"/>
         <location filename="../PreferencesDialog.ui" line="62"/>
         <source>Interface Language:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../Backup/PreferencesDialog-2.0.0.ui" line="85"/>
-        <location filename="../Backup/PreferencesDialog-2.0.0a.ui" line="85"/>
-        <location filename="../Backup/PreferencesDialog-3.0.ui" line="85"/>
-        <location filename="../Backup/PreferencesDialog.3.0.Beta0.ui" line="85"/>
-        <location filename="../PreferencesDialog.ui" line="85"/>
-        <source>Restore original window size</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../Backup/PreferencesDialog-2.0.0.ui" line="111"/>
@@ -43,7 +34,22 @@
         <location filename="../Backup/PreferencesDialog.3.0.Beta0.ui" line="111"/>
         <location filename="../PreferencesDialog.ui" line="111"/>
         <source>Enable Logging</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../Backup/PreferencesDialog-2.0.0.ui" line="137"/>
+        <location filename="../Backup/PreferencesDialog-3.0.ui" line="137"/>
+        <source>Enable jobs history</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../Backup/PreferencesDialog-2.0.0.ui" line="85"/>
+        <location filename="../Backup/PreferencesDialog-2.0.0a.ui" line="85"/>
+        <location filename="../Backup/PreferencesDialog-3.0.ui" line="85"/>
+        <location filename="../Backup/PreferencesDialog.3.0.Beta0.ui" line="85"/>
+        <location filename="../PreferencesDialog.ui" line="85"/>
+        <source>Restore original window size</source>
+        <translation></translation>
     </message>
     <message>
         <location filename="../Backup/PreferencesDialog-2.0.0.ui" line="124"/>
@@ -52,26 +58,7 @@
         <location filename="../Backup/PreferencesDialog.3.0.Beta0.ui" line="124"/>
         <location filename="../PreferencesDialog.ui" line="124"/>
         <source>Font &amp; Size:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../Backup/PreferencesDialog-2.0.0.ui" line="137"/>
-        <location filename="../Backup/PreferencesDialog-3.0.ui" line="137"/>
-        <source>Enable jobs history</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../Backup/PreferencesDialog-2.0.0.ui" line="156"/>
-        <location filename="../Backup/PreferencesDialog-3.0.ui" line="156"/>
-        <location filename="../PreferencesDialog.ui" line="238"/>
-        <source>Enable log viewer</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../Backup/PreferencesDialog-2.0.0.ui" line="195"/>
-        <location filename="../Backup/PreferencesDialog-3.0.ui" line="195"/>
-        <source>Automatically save jobs</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../Backup/PreferencesDialog-2.0.0.ui" line="209"/>
@@ -80,39 +67,52 @@
         <location filename="../Backup/PreferencesDialog.3.0.Beta0.ui" line="226"/>
         <location filename="../PreferencesDialog.ui" line="252"/>
         <source>Restore Defaults</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="../Backup/PreferencesDialog-2.0.0a.ui" line="143"/>
-        <source>Enable Log Viewer</source>
-        <translation type="unfinished"></translation>
+        <location filename="../Backup/PreferencesDialog-2.0.0.ui" line="156"/>
+        <location filename="../Backup/PreferencesDialog-3.0.ui" line="156"/>
+        <location filename="../PreferencesDialog.ui" line="238"/>
+        <source>Enable log viewer</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../Backup/PreferencesDialog-2.0.0.ui" line="195"/>
+        <location filename="../Backup/PreferencesDialog-3.0.ui" line="195"/>
+        <source>Automatically save jobs</source>
+        <translation></translation>
     </message>
     <message>
         <location filename="../Backup/PreferencesDialog-3.0.ui" line="223"/>
         <location filename="../Backup/PreferencesDialog.3.0.Beta0.ui" line="172"/>
         <location filename="../PreferencesDialog.ui" line="172"/>
         <source>Algorithm:</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../Backup/PreferencesDialog-3.0.ui" line="236"/>
         <location filename="../Backup/PreferencesDialog.3.0.Beta0.ui" line="185"/>
         <location filename="../PreferencesDialog.ui" line="185"/>
         <source>0</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../Backup/PreferencesDialog-3.0.ui" line="249"/>
         <location filename="../Backup/PreferencesDialog.3.0.Beta0.ui" line="198"/>
         <location filename="../PreferencesDialog.ui" line="198"/>
         <source>1</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../Backup/PreferencesDialog-3.0.ui" line="262"/>
         <location filename="../Backup/PreferencesDialog.3.0.Beta0.ui" line="211"/>
         <location filename="../PreferencesDialog.ui" line="211"/>
         <source>2</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../Backup/PreferencesDialog-2.0.0a.ui" line="143"/>
+        <source>Enable Log Viewer</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -126,17 +126,17 @@
     <message>
         <location filename="../ProjectInfoOkDialog.ui" line="19"/>
         <source>Project Information</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../ProjectInfoOkDialog.ui" line="47"/>
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../ProjectInfoOkDialog.ui" line="70"/>
         <source>Description</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
@@ -144,18 +144,18 @@
     <message>
         <location filename="../SearchTextDialog.ui" line="19"/>
         <source>Search Text</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../SearchTextDialog.ui" line="47"/>
         <location filename="../SearchTextDialog.ui" line="92"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../SearchTextDialog.ui" line="65"/>
         <source>Text to search:</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 </TS>
