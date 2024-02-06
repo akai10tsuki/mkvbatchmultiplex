ce5e471371ba1ea2eb89125690156d86f3f91847
akai10tuki <akai10tsuki@gmail.com>
2024-02-06 12:54:04 -0500

    Cleanup

--- MKVBatchMultiplex/widgets/JobsOutputErrorsWidget.py
+++ MKVBatchMultiplex/widgets/JobsOutputErrorsWidget.py
@@ -12,18 +12,8 @@ from vsutillib.pyside6 import QOutputTextWidget, TabWidgetExtension
 class JobsOutputErrorsWidget(TabWidgetExtension, QOutputTextWidget):
 
     def __init__(
-        self, 
-        parent: QWidget, 
-        log: Optional[bool] = None, 
+        self,
+        parent: QWidget,
+        log: Optional[bool] = None,
         **kwargs: Any):
         super().__init__(parent=parent, log=log, **kwargs)
-
-"""
-        self.parent = parent
-
-    def clear(self):
-
-        self.parent.progress.lblSetValue.emit(4, 0)
-
-        super().clear()
-"""
