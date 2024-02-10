4708db3e5703652fe9998d245ca4276e3a2b271e
akai10tuki <akai10tsuki@gmail.com>
2024-02-09 21:21:13 -0500

    Change make sure disabled text button text is set to grey for Linux light modes to work.

--- MKVBatchMultiplex/main.py
+++ MKVBatchMultiplex/main.py
@@ -55,6 +55,7 @@ from vsutillib.pyside6 import (
     QOutputTextWidget,
     QProgressIndicator,
     QSystemTrayIconWidget,
+    setAppStyle,
     TabWidget,
     VerticalLine,
 )
@@ -634,7 +635,8 @@ def mainApp():
     config.init(app=app)
     MainWindow()
 
-    app.setStyle("Fusion")
+    setAppStyle(app)
+    #app.setStyle("Fusion")
     #app.setPalette( get_darkModePalette( app ) )
     #darkPalette(app)
     #config.data.set(config.ConfigKey.DarkMode, True)
@@ -643,33 +645,6 @@ def mainApp():
     app.exec()
 
     config.close()
-
-
-def get_darkModePalette( app=None ) :
-
-    darkPalette = app.palette()
-    #darkPalette.setColor( QPalette.Window, QColor( 53, 53, 53 ) )
-    #darkPalette.setColor( QPalette.WindowText, Qt.white )
-    #darkPalette.setColor( QPalette.Disabled, QPalette.WindowText, QColor( 127, 127, 127 ) )
-    #darkPalette.setColor( QPalette.Base, QColor( 42, 42, 42 ) )
-    #darkPalette.setColor( QPalette.AlternateBase, QColor( 66, 66, 66 ) )
-    #darkPalette.setColor( QPalette.ToolTipBase, QColor( 53, 53, 53 ) )
-    #darkPalette.setColor( QPalette.ToolTipText, Qt.white )
-    #darkPalette.setColor( QPalette.Text, Qt.white )
-    #darkPalette.setColor( QPalette.Disabled, QPalette.Text, QColor( 127, 127, 127 ) )
-    #darkPalette.setColor( QPalette.Dark, QColor( 35, 35, 35 ) )
-    #darkPalette.setColor( QPalette.Shadow, QColor( 20, 20, 20 ) )
-    darkPalette.setColor( QPalette.Button, QColor( 53, 53, 53 ) )
-    darkPalette.setColor( QPalette.ButtonText, Qt.white )
-    darkPalette.setColor( QPalette.Disabled, QPalette.ButtonText, QColor( 127, 127, 127 ) )
-    #darkPalette.setColor( QPalette.BrightText, Qt.red )
-    #darkPalette.setColor( QPalette.Link, QColor( 42, 130, 218 ) )
-    #darkPalette.setColor( QPalette.Highlight, QColor( 42, 130, 218 ) )
-    #darkPalette.setColor( QPalette.Disabled, QPalette.Highlight, QColor( 80, 80, 80 ) )
-    #darkPalette.setColor( QPalette.HighlightedText, Qt.white )
-    #darkPalette.setColor( QPalette.Disabled, QPalette.HighlightedText, QColor( 127, 127, 127 ), )
-
-    return darkPalette
 
 # This if for Pylance _() is not defined
 def _(dummy):
